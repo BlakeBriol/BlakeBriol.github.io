b2ad469f6b16717828e1c8c0f1e83844898a5ad1
BlakeBriol <BlakeBriol@gmail.com>
2024-08-16 13:22:22 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -20,24 +20,6 @@ def projects():
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    if request.method == 'POST':
-        # Retrieve form data
-        name = request.form['name']
-        email = request.form['email']
-        message = request.form['message']
-
-        # Validate data
-        if name and email and message:
-            # Open or create CSV file
-            with open(CSV_FILE, 'a', newline='') as file:
-                writer = csv.writer(file)
-                writer.writerow([name, email, message])
-
-            # Redirect to thank you page
-            return redirect(url_for('thank_you'))
-        else:
-            return "All fields are required.", 400
-
     return render_template("contact.html")
 
 @app.route('/thank_you')
