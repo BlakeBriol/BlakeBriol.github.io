df07ae9c4fda409470b7a742522642e97f18fae3
BlakeBriol <BlakeBriol@gmail.com>
2024-08-16 12:53:20 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -18,6 +18,10 @@ def resume():
 def projects():
     return render_template("projects.html")
 
+@app.route('/thank_you')
+def thank_you():
+    return render_template("thank_you.html")
+
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
@@ -40,9 +44,6 @@ def contact():
 
     return render_template("contact.html")
 
-@app.route('/thank_you')
-def thank_you():
-    return render_template("thank_you.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
